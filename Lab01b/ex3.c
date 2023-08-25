9e3aaa026ee25d32fb1edbfce498d14b6b221f46
Cloud User <ec2-user@ip-172-31-38-191.ec2.internal>
2023-08-25 11:34:00 +0000

    exercício 3

--- Lab01b/ex3.c
+++ Lab01b/ex3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void print_sequencia(int inicio, int fim) {
+    for (int i = inicio; i <= fim; i++)
+        printf("%d ", i);
+    printf("\n");
+}
+
+void print_espaco() {
+    printf("  ");
+}
+
 int main() {
-	int entrada = 9;
-	for (int i = 0; i < entrada/2; ++i) {
-		for (int j 
+    int entrada = 9;
+    for (int i = 1; i <= entrada; ++i) {
+        for (int j = 1; j < i; ++j)
+            print_espaco();
+        print_sequencia(i, entrada);
+        --entrada;
+    }
+}
